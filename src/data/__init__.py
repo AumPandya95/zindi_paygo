bda93bfa44c44a4c5e4e74a90c2c602bff31f2a8
Aum Pandya <aumpandya.95@gmail.com>
2021-06-30 21:46:54 +0530

    Fix: Added SubmissionFile class to the list of exposed functions/ classes

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -6,6 +6,7 @@ The final data is then stored in the main data folder's processed directory.
 
 The processed data sets are then used in the jupyter notebooks present in the notebooks directory.
 """
-__all__ = ['one_hot_encoding']
+__all__ = ['one_hot_encoding', 'SubmissionFile']
 
 from src.data.pre_process import one_hot_encoding
+from src.data.validation_file import SubmissionFile
