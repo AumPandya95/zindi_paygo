0d3db34699c6d568c5ed17f31844bc0a76ac3c3e
Aum Pandya <aumpandya.95@gmail.com>
2021-07-09 01:38:38 +0530

    Feat: Hyper-parameter optimisation work flow integrated

--- src/model/tree_based.py
+++ src/model/tree_based.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Dict, Optional
 
 import numpy as np
 import pandas as pd
@@ -18,16 +18,21 @@ class ModelXgBoost:
 
     def train_model(
             self,
+            optimise_model: bool = False,
+            params: Optional[Dict] = None,
             **tuned_params
-    ) -> None:
+    ) -> Union[xgb.XGBRegressor, None]:
         """Train the model on given data using Scikit-learn API of XGBoost."""
         model = xgb.XGBRegressor(use_label_encoder=False,
                                  verbosity=0,
                                  objective="reg:squarederror",
-                                 reg_lambda=0,
-                                 reg_alpha=50)
-        self.trained_model = model.fit(self.train_array, self.train_target)
-        return
+                                 reg_lambda=tuned_params.get('reg_lambda', 0),
+                                 reg_alpha=tuned_params.get('reg_alpha', 50))
+        if not optimise_model:
+            self.trained_model = model.fit(self.train_array, self.train_target)
+            return
+        else:
+            return model
 
     def get_imp_features(
             self
