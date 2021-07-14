6c67bc761cd4b9cdfdc6b6b03085d3aa6810dc27
Nikhil <nikhil.gupta@vphrase.com>
2021-07-14 22:50:18 +0530

    New Pipeline

--- src/features/encoding.py
+++ src/features/encoding.py
@@ -1,3 +1,4 @@
+# from typing import Union, Optional, Tuple
 from typing import Union, Optional, Tuple
 
 import numpy as np
@@ -14,16 +15,21 @@ class FeatureEncoding:
         self.encoder_obj = None
 
     @conv_to_df
-    def one_hot_encoding(self, categorical_frame: Union[np.ndarray, pd.DataFrame], mode: str = "train",
-                         conv: Optional[bool] = True, drop: Optional[str] = None, handle_unknown: str = "ignore"
-                         ) -> Tuple[Union[np.ndarray, pd.DataFrame], Optional[EncoderType]]:
+    def one_hot_encoding(
+            self,
+            categorical_frame: Union[np.ndarray, pd.DataFrame],
+            type_of_data: str = "train",
+            conv: Optional[bool] = True,
+            drop: Optional[str] = None,
+            handle_unknown: str = "ignore"
+    ) -> Tuple[Union[np.ndarray, pd.DataFrame], Optional[EncoderType]]:
         """
         Return the encoded categorical columns.
         Parameters
         ----------
         categorical_frame: np.array
             The categorical features from the train/ test set
-        mode: str :: ["train", "test", "validation"]
+        type_of_data: str :: ["train", "test", "validation"]
             if data is train then the encoder will be fit on the data
         conv: bool
             Whether to convert the encoded array to a dataframe
@@ -35,34 +41,17 @@ class FeatureEncoding:
         -------
         encoded_features: np.array
         """
-        if mode in ("train", "dev"):
+        if type_of_data == "train":
             self.encoder_obj = OneHotEncoder(drop=drop, handle_unknown=handle_unknown)
             self.encoder_obj.fit(categorical_frame)
             final_array = self.encoder_obj.transform(categorical_frame).toarray()
+
             return final_array
         else:
             if self.encoder_obj:
                 final_array = self.encoder_obj.transform(categorical_frame).toarray()
-                return final_array, self.encoder_obj
-
-            raise Exception(
-                "Encoder Object is empty"
-            )
-
-#
-# if __name__ == "__main__":
-#     # frame = pd.DataFrame([[1, 2, 3, 4], [5, 2, 10, 14]], columns=['a', 'b', 'c', 'd'])
-#     frame = np.array([[1, 2, 3, 4], [5, 2, 10, 14]])
-#     fin, enc = one_hot_encoding(categorical_frame=frame, conv=True)
-#     print(list(fin.columns))
-#     # 1 5 2 3 10 4 14
-#
-#     fin, enc = one_hot_encoding(categorical_frame=frame, conv=False)
-#     print(fin)
-#
-#     fr, en = one_hot_encoding(categorical_frame=np.array([[6, 2, 10, 14],
-#                                                           [1, 2, 3, 14]]),
-#                               mode='test',
-#                               encoder_obj=enc,
-#                               conv=True)
-#     print(fr)
+                return final_array
+            else:
+                raise Exception(
+                    "No fitted encoder object provided to transform the test/ validation data set."
+                )
