6ecd96e0289fc607b7e2403304b180e33b45ade2
Aum Pandya <aumpandya.95@gmail.com>
2021-07-29 23:31:15 +0530

    Chore: updated exposed classes

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -14,8 +14,14 @@ __all__ = [
     'std_dev_calc',
     'back_feature',
     'add_predicted_payment',
-    'convert'
+    'convert',
+    'FeatureEncoding',
+    'FeatureEngineering',
+    'FeatureImputation'
 ]
 
 from src.features.utils import split, length_calc, sum_calc, mean_calc, median_calc, max_calc, min_calc, std_dev_calc, \
     back_feature, add_predicted_payment, convert
+from src.features.encoding import FeatureEncoding
+from src.features.engineering import FeatureEngineering
+from src.features.imputation import FeatureImputation
